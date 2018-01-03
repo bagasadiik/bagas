9dd8f95d8c6c83b76ef73dec5f8a4fc417f4fc0a
bagasadiik <34002286+bagasadiik@users.noreply.github.com>
2018-01-04 02:08:15 +0700

    Update bete.php

--- bete.php
+++ bete.php
@@ -3,12 +3,12 @@ error_reporting(0);
 if(isset($_GET["btc"])){
 	$add = $_GET["btc"];
 } else {
-echo "17uE9thFWV4bDYKof9zNHhZ9q2Eef2cQn3";
+echo "MANA BTC LO";
 exit();
 }
 $cookie = tempnam('tmp','avo'.rand(1000000,9999999).'tmp.txt');
 $betece = $_GET["btc"];
-$i = curl("https://www.thebestbitcoinfaucet.com/","address={$17uE9thFWV4bDYKof9zNHhZ9q2Eef2cQn3}&sponsor=",0,1,'','',0,'','',$cookie,0);
+$i = curl("https://www.thebestbitcoinfaucet.com/","address={$betece}&sponsor=",0,1,'','',0,'','',$cookie,0);
 $true = get_between($i,'<p style="color:#CE224D"><b>',' : '.$betece.'</p>');
 if($true == "Wallet"){
 $i = curl("https://www.thebestbitcoinfaucet.com/","",0,1,'','',0,'','',$cookie,0);
